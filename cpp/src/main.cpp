848e12708d084b2ce631ecfb667b8c05eb432b13
seunghyek.cho <shhj1998@postech.ac.kr>
2019-11-21 20:01:42 +0900

    [C++] #3 Simple HTTP server with JSON parsing

--- cpp/src/main.cpp
+++ cpp/src/main.cpp
@@ -1,12 +1,33 @@
 #include <httplib.h>
+#include <json/json.h>
+#include <vector>
+
+using namespace httplib;
+
+void sample(const Request& req, Response& res) {
+    Json::Value root;
+    std::stringstream stream;
+    std::vector<std::vector<int> > v;
+
+    stream.str(req.body);
+    stream >> root;
+
+    for(const Json::Value& input : root["input"]) {
+        std::vector<int> result;
+
+        for (const Json::Value &element: input) {
+            result.push_back(element.asInt());
+        }
+
+        v.push_back(result);
+    }
+
+    res.set_content(req.body, "application/json");
+}
 
 int main() {
-    using namespace httplib;
-
     Server server;
-    server.Get("/", [](const Request& req, Response& res) {
-        res.set_content("Hello, World!", "text/plain");
-    });
+    server.Post("/", sample);
 
     server.listen("localhost", 8080);
 
